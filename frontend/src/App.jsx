c3914eaf2a962da76935164d02a0c0bb75e15259
ravijar <ravijarandinu@gmail.com>
2023-09-19 19:06:11 +0530

    added navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DrawerComponent from './shared/components/DrawerComponent'
 import AddOrderPage from './addOrder/pages/AddOrderPage';
 import RestaurantsPage from './restaurants/pages/RestaurantsPage';
+import WalletPage from './wallet/pages/WalletPage'
 
 function App() {
   return (
@@ -9,8 +10,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<DrawerComponent />} >
-            <Route path="/addOrder" element={<AddOrderPage />} />?
-            <Route path="/shops" element={<RestaurantsPage />} />?
+            <Route path="addOrder" element={<AddOrderPage />} />
+            <Route path="shops" element={<RestaurantsPage />} />
+            <Route path="wallet" element={<WalletPage />} />
           </Route>
         </Routes>
       </Router>
