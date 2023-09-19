27e0a4be9c0b5fa70fedd865734bf24c2c6d7aef
Prasanjith76 <prasanjith123.lorensuhewa@gmail.com>
2023-09-19 18:49:20 +0530

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import DrawerComponent from './shared/components/DrawerComponent'
+import AddOrderPage from './addOrder/pages/AddOrderPage';
+import RestaurantsPage from './restaurants/pages/RestaurantsPage';
+import WalletPage from './wallet/pages/WalletPage';
 
 function App() {
   return (
     <>
       <DrawerComponent />
+      <Router>
+        <Routes>
+          <Route path="/" element={<DrawerComponent />} >
+            <Route path="/addOrder" element={<AddOrderPage />} />?
+            <Route path="/shops" element={<RestaurantsPage />} />?
+            <Route path="/wallet" element={<WalletPage />} />?
+          </Route>
+        </Routes>
+      </Router>
     </>
   )
 }
